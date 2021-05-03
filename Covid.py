059887755d48034840a36eab616f59fcac12ad0a
joel <joelraj97@gmail.com>
2021-05-03 16:29:48 +0530

    fourth commit

--- Covid.py
+++ Covid.py
@@ -4,69 +4,70 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-
-
-
 from dash.dependencies import Output,Input
-app=dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
-
-
-
-df=pd.read_csv("owid-covid-data(1).csv")
-
-
+app=dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+df=pd.read_csv("owid-covid-data(1).csv")   #raed data from csv
+print(df)
+app.head = html.Link(rel='stylesheet', href='./static/stylesheet.css'),
 #App Layout
 app.layout=html.Div([
     dbc.Button("Success",color="success",className="mr-1"),
-                                                                      
-
-    html.H1("Covid-19 Coronavirus Pandemic",style={"text-align":"center"}),
-
-
-    dcc.Dropdown(id="my_option",
+    html.H1("Covid-19 Coronavirus Pandemic",style={"text-align":"center"}), #heading of the application
+    dcc.Dropdown(id="my_option",       #drop down menu
                  options=[{'label':i,'value':i}
-                          for i in df["location"].unique()],
-                 value="Afghanistan",
-                 style={'size': 3, "offset": 2, 'order': 3,"color":"Red","width":"50%"},
-                 multi=False,
-
-
-                 ),
+                          for i in df["location"].unique()],   #selecting options from the list of locations
+                 value="Afghanistan",   #initila value of the dropdown
+                 style={'size': 3, "offset": 2, 'order': 3,"color":"Red","width":"50%"},#style features of the dropdown
+                 multi=False,   #wheather multiple values allowed in the dropdown
+                ),
     html.Br(),
-    html.Div(id="dateid",style={"text-align":"left","font-size":50,"color":"Blue"}),
+    html.Div(id="dateid",style={"text-align":"left","font-size":50,"color":"Blue"}),  #latest date of the available data
     html.Br(),
-    dbc.Row(
-
-        [
-            dbc.Col(   [
-
-                 html.H2("Total Cases",style={"text-align":"center"}),
-                 html.Div(id="totalcases",style={'size': 3, "offset": 2, 'order': 3,"color":"Red","text-align":"center","font-size":90})],
-                 width={'size': 3, "offset": 0, 'order': 3}
+    dbc.Container([
+    dbc.Row(    #Row1
+     [
+            dbc.Col(   [    #first column of Row 1
+            dbc.Alert([
+                  html.H2("Total Cases",style={"text-align":"center"}),
+                 html.Div(id="totalcases",style={'size': 3, "offset": 2, 'order': 3,"color":"Red","text-align":"center","font-size":90})])],
+                 width={'size': 5, "offset": 0, 'order': 3}
         ),
 
-            dbc.Col( [
+           dbc.Col(   [    #first column of Row 1
+            dbc.Alert([
+                  html.H2("Total Cases Per ",style={"text-align":"center"}),
+                 html.Div(id="totalcasesper",style={'size': 3, "offset": 2, 'order': 3,"color":"Red","text-align":"center","font-size":90})])],
+                 width={'size': 4, "offset": 0, 'order': 3}
+        ),
+
+            dbc.Col( [     #second column of Row 1
+              dbc.Alert([
                 html.H2("Deaths",style={"text-align":"center"}),
-                html.Div(id="deathno",title="Deaths",draggable="true",style={'size': 6, "offset": 2, 'order': 3,"color":"Red","text-align":"center","font-size":90})],
+                html.Div(id="deathno",title="Deaths",draggable="true",style={'size': 6, "offset": 2, 'order': 3,"color":"Red","text-align":"center","font-size":90})])],
                 width={'size': 3, "offset": 0, 'order': 3}
             )
             ]) ,
+        ]),
     html.Br(),
-    dbc.Row( [
-        dbc.Col(
+    dbc.ModalBody([
+    dbc.Row( [    #Row 2
+        dbc.Col(    #First column of row 2
 
     dcc.Graph(id="linegraph2",figure={})    ,
     width={'size': 6, "offset": 0, 'order': 2}
             
 
         ) ,
-        dbc.Col(
+        dbc.Col(       #Secon Column of Row 2
           dcc.Graph(id="piechart",figure={})    ,
             width={'size': 5, "offset": 0, 'order': 2}
         )    ,
 
 
-   ] )
+   ] )      ,
+]),
+   html.Br(),
+   dcc.Graph(id="line",figure={})  ,
 ])
 
 #call back
@@ -74,19 +75,22 @@ app.layout=html.Div([
 @app.callback(
      [Output(component_id="dateid",component_property="children"),
       Output(component_id="totalcases",component_property="children"),
+      Output(component_id="totalcasesper",component_property="children"),
      Output(component_id="deathno",component_property="children"),
      Output(component_id="linegraph2",component_property="figure") ,
-      Output(component_id="piechart",component_property="figure")],
+      Output(component_id="piechart",component_property="figure") ,
+      Output(component_id="line",component_property="figure")],
     Input(component_id="my_option",component_property="value")
 )
 
 
 def update_graph(option_slctd):
 
-    filterdata=df[df["location"]==option_slctd]
-    totalcases=int(filterdata["new_cases"].sum())
-    deaths=int(filterdata["new_deaths"].sum())
-    dates=filterdata["date"].tail(1)
+    filterdata=df[df["location"]==option_slctd]    #to filter out data for the selected country
+    totalcases=int(filterdata["new_cases"].sum())         #to find the total cases in the selected country
+    totalcasesper=int(filterdata["total_cases_per_million"].sum())
+    deaths=int(filterdata["new_deaths"].sum())   #to find the deaths in the selsected country
+    dates=filterdata["date"].tail(1)     #to return the latest value of date in the selected country
     index=dates.index.values
     print(dates)
     strings = [str(integer) for integer in index]
@@ -116,9 +120,10 @@ def update_graph(option_slctd):
         width=700,
         height=300,
         paper_bgcolor="LightSteelBlue",
-    )
 
-    return "Data Upto: "+dates, totalcases,deaths,fig2,piegraph
+    )
+    pie=px.line(filterdata,x="total_cases",y="new_cases_smoothed")
+    return "Data Upto: "+dates, totalcases,totalcasesper,deaths,fig2,piegraph ,pie
 
 if __name__ == '__main__':
     app.run_server(debug=True)         
